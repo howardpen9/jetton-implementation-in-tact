008fd40e2978af5026db0995d97bab71a1e3425c
ton.gggg <howard.pen9@gmail.com>
2024-02-19 09:47:24 +0900

    uuuu

--- sources/contract.spec.ts
+++ sources/contract.spec.ts
@@ -8,17 +8,16 @@ import {
     RemoteBlockchainStorage,
     wrapTonClient4ForRemote,
 } from "@ton/sandbox";
-
 import "@ton/test-utils";
-// import "@ton-community/test-utils";
-
-// import { Address, beginCell, fromNano, StateInit, toNano } from "ton-core";
 import { Address, beginCell, fromNano, StateInit, toNano } from "@ton/core";
 import { TonClient4 } from "@ton/ton";
+import { printSeparator } from "./utils/print";
 
+// -------- Contract SDK --------
 import { SampleJetton, Mint, TokenTransfer } from "./output/SampleJetton_SampleJetton";
 import { JettonDefaultWallet, TokenBurn } from "./output/SampleJetton_JettonDefaultWallet";
 
+// -------- DeDust.io SDK --------
 import {
     Asset,
     Factory,
@@ -30,7 +29,10 @@ import {
     JettonRoot,
     ReadinessStatus,
 } from "@dedust/sdk";
-import { printSeparator } from "./utils/print";
+
+// ------------ STON.fi SDK ------------
+import TonWeb from "tonweb";
+import { Router, ROUTER_REVISION, ROUTER_REVISION_ADDRESS } from "@ston-fi/sdk";
 
 const jettonParams = {
     name: "Best Practice",
@@ -70,7 +72,7 @@ describe("contract", () => {
         jettonWallet = blockchain.openContract(await JettonDefaultWallet.fromAddress(playerWallet));
     });
 
-    it("should deploy", async () => {
+    it("Test: whether contract deployed successfully", async () => {
         // the check is done inside beforeEach, blockchain and token are ready to use
         // console.log((await token.getGetJettonData()).owner);
         // console.log((await token.getGetJettonData()).totalSupply);
@@ -78,7 +80,7 @@ describe("contract", () => {
         // console.log((await token.getGetJettonData()).content);
     });
 
-    it("should mint successfully", async () => {
+    it("Test: Minting is successfully", async () => {
         const totalSupplyBefore = (await token.getGetJettonData()).total_supply;
         const mintAmount = toNano(100);
         const Mint: Mint = {
@@ -92,7 +94,7 @@ describe("contract", () => {
             to: token.address,
             success: true,
         });
-        printTransactionFees(mintResult.transactions);
+        // printTransactionFees(mintResult.transactions);
 
         const totalSupplyAfter = (await token.getGetJettonData()).total_supply;
         expect(totalSupplyBefore + mintAmount).toEqual(totalSupplyAfter);
@@ -155,7 +157,7 @@ describe("contract", () => {
         const deployerWalletAddress = await token.getGetWalletAddress(deployer.address);
         const deployerWallet = blockchain.openContract(JettonDefaultWallet.fromAddress(deployerWalletAddress));
         let deployerBalanceInit = (await deployerWallet.getGetWalletData()).balance;
-        console.log("deployerBalanceInit = ", deployerBalanceInit);
+
         const initMintAmount = toNano(100);
         const mintMessage: Mint = {
             $$type: "Mint",
@@ -175,7 +177,7 @@ describe("contract", () => {
             custom_payload: beginCell().endCell(),
         };
 
-        const burnResult = await deployerWallet.send(deployer.getSender(), { value: toNano("10") }, burnMessage);
+        await deployerWallet.send(deployer.getSender(), { value: toNano("10") }, burnMessage);
         let deployerBalanceAfterBurn = (await deployerWallet.getGetWalletData()).balance;
         expect(deployerBalanceAfterBurn).toEqual(deployerBalance - burnAmount);
     });
@@ -198,23 +200,24 @@ describe("contract", () => {
         });
         let totalSupply_later = (await token.getGetJettonData()).total_supply;
         expect(totalSupply_later).toEqual(totalSupply);
+
         // printTransactionFees(messateResult.transactions);
         // prettyLogTransactions(messateResult.transactions);
     });
 
-    // it("Convert Address Format", async () => {
-    //     console.log("Example Address(Jetton Root Contract: " + token.address);
-    //     console.log("Is Friendly Address: " + Address.isFriendly(token.address.toString()));
+    it("Convert Address Format", async () => {
+        console.log("Example Address(Jetton Root Contract: " + token.address);
+        console.log("Is Friendly Address: " + Address.isFriendly(token.address.toString()));
 
-    //     const testAddr = Address.parse(token.address.toString());
-    //     console.log("✓ Address: " + testAddr.toString({ bounceable: false }));
-    //     console.log("✓ Address: " + testAddr.toString());
-    //     console.log("✓ Address(urlSafe: true): " + testAddr.toString({ urlSafe: true }));
-    //     console.log("✓ Address(urlSafe: false): " + testAddr.toString({ urlSafe: false }));
-    //     console.log("✓ Raw Address: " + testAddr.toRawString());
-    // });
+        const testAddr = Address.parse(token.address.toString());
+        console.log("✓ Address: " + testAddr.toString({ bounceable: false }));
+        console.log("✓ Address: " + testAddr.toString());
+        console.log("✓ Address(urlSafe: true): " + testAddr.toString({ urlSafe: true }));
+        console.log("✓ Address(urlSafe: false): " + testAddr.toString({ urlSafe: false }));
+        console.log("✓ Raw Address: " + testAddr.toRawString());
+    });
 
-    it("Onchian Testing", async () => {
+    it("Onchian Testing: DeDust", async () => {
         const blkch = await Blockchain.create({
             storage: new RemoteBlockchainStorage(
                 wrapTonClient4ForRemote(
@@ -239,7 +242,7 @@ describe("contract", () => {
         const targetBalances: [bigint, bigint] = [tonAmount, scaleAmount];
         console.log("DeDust Factory Address: " + MAINNET_FACTORY_ADDR);
 
-        // 0x21cfe02b / 567271467: Create Vault
+        // Step 1: 0x21cfe02b / 567271467: Create Vault
         // https://docs.dedust.io/reference/tlb-schemes#message-create_vault
         const factory = blkch.openContract(Factory.createFromAddress(MAINNET_FACTORY_ADDR));
         const Tx = await factory.sendCreateVault(player.getSender(), {
@@ -248,7 +251,7 @@ describe("contract", () => {
         await printTransactionFees(await Tx.transactions);
 
         // ------------------------------------------------------------------------------------------------
-        // (0x97d51f2f / 2547326767: Create a volatile pool)
+        // Step 2: 0x97d51f2f / 2547326767: Create a volatile pool)
         // https://docs.dedust.io/reference/tlb-schemes#message-create_volatile_pool
         const pool = blkch.openContract(await factory.getPool(PoolType.VOLATILE, [TON, SCALE]));
 
@@ -261,7 +264,7 @@ describe("contract", () => {
         }
 
         // ------------------------------------------------------------------------------------------------
-        // 0xd55e4686, Deposit / Adding Liquidity: Deposit TON to Vault
+        // Step 3-1: 0xd55e4686, Deposit / Adding Liquidity: Deposit TON to Vault
         // https://docs.dedust.io/reference/tlb-schemes#message-deposit_liquidity
         const tonVault = blkch.openContract(await factory.getNativeVault());
         console.log("Native Vault Address: " + tonVault.address);
@@ -273,7 +276,7 @@ describe("contract", () => {
         });
         await printTransactionFees(await tx.transactions);
 
-        // Deposit Jetton to Vault
+        // Step 3-2: Deposit Jetton to Vault
         const scaleRoot = blkch.openContract(JettonRoot.createFromAddress(jettonRoot.address));
         const scaleWallet = blkch.openContract(await scaleRoot.getWallet(player.address));
         await jettonRoot.send(player.getSender(), { value: toNano("10") }, "Mint: 100");
@@ -304,16 +307,16 @@ describe("contract", () => {
             throw new Error("Vault (TON) does not exist.");
         }
 
-        // Swap TON to Jetton
+        // Step 4-1: 0xea06185d Swap TON to Jetton
         const amountIn = toNano("0.0001"); // 5 TON
         const swapTx_result = await tonVault.sendSwap(player.getSender(), {
             poolAddress: pool.address,
             amount: amountIn,
-            gasAmount: toNano("0.25"),
+            gasAmount: toNano("0.252"),
         });
         await printTransactionFees(await swapTx_result.transactions);
 
-        // Swap Jetton to TON
+        // Swap 4-2: 0xf8a7ea5 Jetton to TON 0xf8a7ea5
         const jettonAmountIn = toNano("0.00000001"); // 50 SCALE
         const swapJetton_result = await scaleWallet.sendTransfer(player.getSender(), toNano("0.3030303"), {
             amount: jettonAmountIn,
@@ -325,10 +328,11 @@ describe("contract", () => {
                 swapParams: { recipientAddress: deployer.address },
             }),
         });
-        await printTransactionFees(await swapJetton_result.transactions);
+        // await printTransactionFees(await swapJetton_result.transactions);
+        // await prettyLogTransactions(await swapJetton_result.transactions);
 
         // ------------------------------------------------------------------------------------------------
-        // Remove Liquidity
+        // Step 5: Remove Liquidity
         // https://docs.dedust.io/docs/liquidity-provisioning#withdraw-liquidity
         const lpWallet = blkch.openContract(await pool.getWallet(player.address));
 
@@ -337,7 +341,134 @@ describe("contract", () => {
         });
         console.log("removeTx_Result: ");
         await printTransactionFees(await removeTx_Result.transactions);
+
         console.log("JettonWallet: " + scaleWallet.address);
-        await prettyLogTransactions(await removeTx_Result.transactions);
+        // await prettyLogTransactions(await removeTx_Result.transactions);
     }, 10000);
+
+    it("Onchian Testing: STON.fi", async () => {
+        const blkch = await Blockchain.create({
+            storage: new RemoteBlockchainStorage(
+                wrapTonClient4ForRemote(
+                    new TonClient4({
+                        endpoint: "https://mainnet-v4.tonhubapi.com",
+                    })
+                )
+            ),
+        });
+
+        const provider = new TonWeb.HttpProvider();
+
+        const router = new Router(provider, {
+            revision: ROUTER_REVISION.V1,
+            address: ROUTER_REVISION_ADDRESS.V1,
+        });
+
+        // const router = new Router(blkch, {
+        //     revision: ROUTER_REVISION.V1,
+        //     address: ROUTER_REVISION_ADDRESS.V1,
+        // });
+
+        console.log("Router Address: " + router.address);
+
+        // const JETTON0 = "EQDQoc5M3Bh8eWFephi9bClhevelbZZvWhkqdo80XuY_0qXv";
+        // const JETTON1 = "EQC_1YoM8RBixN95lz7odcF3Vrkc_N8Ne7gQi7Abtlet_Efi";
+        // const pool = await router.getPool({ jettonAddresses: [JETTON0, JETTON1] });
+        // console.log((await pool!!.getData()).protocolFeeAddress);
+
+        const OWNER_ADDRESS = "";
+        const JETTON0 = "EQDQoc5M3Bh8eWFephi9bClhevelbZZvWhkqdo80XuY_0qXv";
+        const JETTON1 = "EQC_1YoM8RBixN95lz7odcF3Vrkc_N8Ne7gQi7Abtlet_Efi";
+
+        const routerData = await router.getData();
+        const { isLocked, adminAddress, tempUpgrade, poolCode, jettonLpWalletCode, lpAccountCode } = routerData;
+
+        const pool = await router.getPool({ jettonAddresses: [JETTON0, JETTON1] });
+
+        if (!pool) {
+            throw Error(`Pool for ${JETTON0}/${JETTON1} not found`);
+        }
+
+        const poolAddress = await pool.getAddress();
+
+        const poolData = await pool.getData();
+        const {
+            reserve0,
+            reserve1,
+            token0WalletAddress,
+            token1WalletAddress,
+            lpFee,
+            protocolFee,
+            refFee,
+            protocolFeeAddress,
+            collectedToken0ProtocolFee,
+            collectedToken1ProtocolFee,
+        } = poolData;
+
+        const expectedLiquidityData = await pool.getExpectedLiquidity({
+            jettonAmount: new TonWeb.utils.BN(500000000),
+        });
+
+        const { amount0, amount1 } = expectedLiquidityData;
+
+        const expectedLpTokensAmount = await pool.getExpectedTokens({
+            amount0: new TonWeb.utils.BN(500000000),
+            amount1: new TonWeb.utils.BN(200000000),
+        });
+
+        if (token0WalletAddress) {
+            const expectedOutputsData = await pool.getExpectedOutputs({
+                amount: new TonWeb.utils.BN(500000000),
+                jettonWallet: token0WalletAddress,
+            });
+            const { jettonToReceive, protocolFeePaid, refFeePaid } = expectedOutputsData;
+        }
+
+        const lpAccountAddress = await pool.getLpAccountAddress({
+            ownerAddress: OWNER_ADDRESS,
+        });
+
+        const lpAccount = await pool.getLpAccount({ ownerAddress: OWNER_ADDRESS });
+        if (lpAccount) {
+            const lpAccountData = await lpAccount.getData();
+            const { userAddress, poolAddress, amount0, amount1 } = lpAccountData;
+        }
+    });
 });
+
+// it("should interact with STON.fi router and pool contracts", async () => {
+//     const OWNER_ADDRESS = "YOUR WALLET ADDRESS HERE";
+//     const JETTON0 = "EQDQoc5M3Bh8eWFephi9bClhevelbZZvWhkqdo80XuY_0qXv";
+//     const JETTON1 = "EQC_1YoM8RBixN95lz7odcF3Vrkc_N8Ne7gQi7Abtlet_Efi";
+
+//     const routerData = await router.getData();
+//     const pool = await router.getPool({ jettonAddresses: [JETTON0, JETTON1] });
+
+//     if (!pool) {
+//         throw new Error(`Pool for ${JETTON0}/${JETTON1} not found`);
+//     }
+
+//     const poolData = await pool.getData();
+//     const expectedLiquidityData = await pool.getExpectedLiquidity({
+//         jettonAmount: new tonWeb.utils.BN(toNano("1")),
+//     });
+
+//     const { amount0, amount1 } = expectedLiquidityData;
+
+//     const lpAccountAddress = await pool.getLpAccountAddress({ ownerAddress: OWNER_ADDRESS });
+//     const lpAccount = await pool.getLpAccount({ ownerAddress: OWNER_ADDRESS });
+
+//     if (lpAccount) {
+//         const lpAccountData = await lpAccount.getData();
+//     }
+
+//     // Assertions to verify the data received from STON.fi SDK
+//     expect(poolData).toBeDefined();
+//     expect(expectedLiquidityData).toBeDefined();
+//     expect(lpAccountAddress).toBeDefined();
+
+//     if (lpAccount) {
+//         expect(lpAccountData).toBeDefined();
+//     }
+// });
+// });
